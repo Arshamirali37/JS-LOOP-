06962956c23fb1c6e1e4d7b93c1caca9ea97e789
ARSHamirali <arshamirali604@gmail.com>
2024-09-26 15:01:13 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,69 @@
+//                                 {FOR LOOP}
+
+//          (Flags)
+
+// var Userinput = prompt("Enter your city name")
+// var citiesArr = ["karachi", "lahore", "mutlan", "quetta", "Peshawar", "Islambad",]
+// var Flag = false
+
+// for (var i = 0; i <= 5; i++) {
+//     if (Userinput === citiesArr[i]) {
+//         console.log("Value match")
+//         Flag = true
+//   }
+// }
+// if (Flag === false) {
+//     console.log("Value not found")
+// }
+
+//           Example:-
+// if (Userinput === citiesArr[0]) {
+//     console.log("Value match")
+// }
+// else if (Userinput === citiesArr[1]) {
+//     console.log("Value match")
+// }
+// else if (Userinput === citiesArr[2]) {
+//     console.log("Value match")
+// }
+// else if (Userinput === citiesArr[3]) {
+//     console.log("Value match")
+// }
+// else if (Userinput === citiesArr[4]) {
+//     console.log("Value match")
+// }
+// else {
+//     console.log("Value not found")
+// }
+
+
+//        (Nested Loop)
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+// }
+// console.log(fullNames)
+
+
+//         (Case Changing)
+
+// var Userinput = prompt("Enter your city name")
+// Userinput =Userinput.toLowerCase()
+// console.log(Userinput)
+// var citiesArr = ["karachi", "lahore", "mutlan", "quetta", "Peshawar", "Islambad",]
+// var Flag = false
+
+// for (var i = 0; i <= 5; i++) {
+//     if (Userinput === citiesArr[i]) {
+//         console.log("Value match")
+//         Flag = true
+//   }
+// }
+// if (Flag === false) {
+//     console.log("Value not found")
+// }
